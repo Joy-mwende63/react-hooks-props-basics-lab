3558233a0c217419bdbe7bb5e42b37525eaee80c
Joy-mwende63 <mwendejoy47@gmail.com>
2024-11-18 22:14:24 +0300

    react-hooks-props-basics-lab

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,11 @@ function About() {
       <h2>About Me</h2>
       <p>I made this!</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <h3>Links</h3>
-      <ul>https://github.com/liza</ul>
-      <ul>https://www.linkedin.com/in/liza/</ul>
+      <h3>Links
+      <a>github="https://github.com/liza"</a>
+      <a>linkedin="https://www.linkedin.com/in/liza/" </a>
+      </h3>
+      
     </div>
   );
 }
